2b1c20e715f358073390e6f35ae1b47794bc289e
alessio.englert <alessio.englert@bluewin.ch>
2023-03-11 09:44:26 +0100

    feature: added some little styles

--- client/styles/MainStyles.php
+++ client/styles/MainStyles.php
@@ -1,6 +1,5 @@
-
 <style>
-  <?php 
+  <?php
   /* Import all css files */
   require_once('ImageBigStyle.css');
   require_once('LoginStyle.css');
@@ -10,41 +9,40 @@
   require_once('SettingsStyle.css');
   require_once('UploadedImageStyle.css');
   require_once('VotedImageStyle.css');
-?>
+  ?>
 
-/* MAIN STYLES */
-.body {
-  background: rgb(255,0,211);
-  background: linear-gradient(90deg, rgba(255,0,211,1) 0%, rgba(255,127,0,1) 81%, rgba(255,192,0,1) 100%);
-  background-attachment: fixed;
-}
+  /* MAIN STYLES */
+  .body {
+    background: rgb(255, 0, 211);
+    background: linear-gradient(90deg, rgba(255, 0, 211, 1) 0%, rgba(255, 127, 0, 1) 81%, rgba(255, 192, 0, 1) 100%);
+    background-attachment: fixed;
+  }
 
-.input {
-  background-color: initial;
-  border-radius: 10px 10px 10px 10px;
-  padding-top: 0.3em;
-  padding-bottom: 0.3em;
-  padding-left: 0.3em;
-  padding-right: 0.3em;
-  border-color: black;
-}
+  .input {
+    background-color: initial;
+    border-radius: 10px 10px 10px 10px;
+    padding-top: 0.3em;
+    padding-bottom: 0.3em;
+    padding-left: 0.3em;
+    padding-right: 0.3em;
+    border-color: black;
+  }
 
-input:focus {
-  outline-width: 0;
-  outline: none;
-}
+  input:focus {
+    outline-width: 0;
+    outline: none;
+  }
 
-.button  {
-  cursor: pointer;
-  border-radius: 10px 10px 10px 10px;
-  background-color: initial;
-  border-color: black;
-  height: 3em;
-}
+  .button {
+    cursor: pointer;
+    border-radius: 10px 10px 10px 10px;
+    background-color: initial;
+    border-color: black;
+    height: 3em;
+  }
 
 
-.button:active {
-  background-color: #0000003b;
-}
-
+  .button:active {
+    background-color: #0000003b;
+  }
 </style>
